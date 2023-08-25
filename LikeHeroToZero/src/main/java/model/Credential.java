801bf697a55eb76589a08f2fed6f5a50134bbd25
Sono Willbrand <sono.willbrand@daubit.org>
2023-08-25 20:02:24 +0200

    chore: rename file

--- LikeHeroToZero/src/main/java/model/Credential.java
+++ LikeHeroToZero/src/main/java/model/Credential.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
-import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -15,16 +13,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 
 @Named
 @SessionScoped
 @Entity
-public class Credentials implements Serializable {
+public class Credential implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "user_id")
+	@Column(name = "owner_id")
 	private Integer id;
 
 	@Column(name = "username", nullable = false, unique = true)
